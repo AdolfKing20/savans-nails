529153f025589b9c5276e961ec5c29d0d1c02ad3
RodrigoGimenez <rodrigogfxs@gmail.com>
2023-11-19 19:16:28 -0300

    ...

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ import { FaUserCircle } from "react-icons/fa";
 
 function NavBar() {
   return (
-    <nav className='bg-[#7209B7] px-2 w-screen flex justify-around items-center fixed bottom-0 pb-3'>
+    <nav className='bg-[#7209B7] px-2 w-screen flex justify-around items-center fixed bottom-0 pb-2'>
         <div className='p-4 bg-[#AD5BA0] rounded-br-lg rounded-bl-lg'>
             <Link href={'/'} className='text-3xl text-slate-100 '>
             <IoHomeSharp/>
