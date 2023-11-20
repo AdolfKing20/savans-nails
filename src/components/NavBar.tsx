af26d3b92e6dc8c5859fc7db1d31fdafc109ba08
RodrigoGimenez <rodrigogfxs@gmail.com>
2023-11-19 21:58:43 -0300

    Carrito

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import { useState } from 'react';
 import Link from 'next/link'
 import { IoHomeSharp } from "react-icons/io5";
 import { FaCartShopping, FaBagShopping  } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
 
-
 function NavBar() {
+
+
+  const [clickStyle, setClickStyle] = useState(false);
+  const styleA1 = 'p-3 bg-[#AD5BA0] rounded-br-lg rounded-bl-lg'
+
+  const handleClick = () => {
+    setClickStyle(!clickStyle);
+  }
+
+
   return (
     <nav className='bg-[#7209B7] px-2 w-screen flex justify-around items-center fixed bottom-0 pb-2 rounded-tl-xl rounded-tr-xl'>
-        <div className='p-3 bg-[#AD5BA0] rounded-br-lg rounded-bl-lg'>
+
+
+        <div className={clickStyle ? styleA1 : ''}>
                 <Link href={'/'} className='text-3xl text-slate-100'>
                 <IoHomeSharp/>
                 </Link>
